8e1e0062d35fb130cff9e9a41261ed2d19234358
Bartek <bartlomiej00wilk@gmail.com>
2023-05-11 21:21:41 +0200

    Initial commit

---
+++ app/src/main/java/wolf/north/paintwithme/MainActivity.kt
@@ -0,0 +1,11 @@
+package wolf.north.paintwithme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
