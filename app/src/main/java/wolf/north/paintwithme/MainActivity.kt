c2cadbd4e167ab892c80d90590e057b704069b75
Bartek <bartlomiej00wilk@gmail.com>
2023-05-14 16:15:10 +0200

    XML STWORZENIE IKON ROZMIARÓW PIÓRA DODANIE DO MAIN FUNKCJONALNOŚCI KTÓRA POZWOLI NA WYBRANIE ROZMIARU PIÓRA OKNO DIALOGOWE - NOWE dialog_brush_size.xml

--- app/src/main/java/wolf/north/paintwithme/MainActivity.kt
+++ app/src/main/java/wolf/north/paintwithme/MainActivity.kt
@@ -1,7 +1,9 @@
 package wolf.north.paintwithme
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         drawingView = findViewById(R.id.drawing_view)
-        drawingView?.setBrushSize(20.toFloat())
+        drawingView?.setBrushSize(20.toFloat()) //DEFAULT BRUSH SIZE ON START
     }
+
+    private fun displayBrushSizeDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size) // SET HOW DIALOG SHOULD LOOK (CREATED LAYOUT)
+        brushDialog.setTitle("Brush size: ")
+        val smallImgBttn : ImageView = brushDialog.findViewById(R.id.imgbttn_brush_small)
+        smallImgBttn.setOnClickListener {
+            drawingView?.setBrushSize(10.toFloat()) //CHANGING BRUSH SIZE
+            brushDialog.dismiss() // HIDE DIALOG AFTER CLICKING ON SMALL BRUSH SIZE
+        }
+        val mediumImgBttn : ImageView = brushDialog.findViewById(R.id.imgbttn_brush_medium)
+        mediumImgBttn.setOnClickListener {
+            drawingView?.setBrushSize(20.toFloat()) //CHANGING BRUSH SIZE
+            brushDialog.dismiss() // HIDE DIALOG AFTER CLICKING ON MEDIUM BRUSH SIZE
+        }
+        val bigImgBttn : ImageView = brushDialog.findViewById(R.id.imgbttn_brush_big)
+        bigImgBttn.setOnClickListener {
+            drawingView?.setBrushSize(30.toFloat()) //CHANGING BRUSH SIZE
+            brushDialog.dismiss() // HIDE DIALOG AFTER CLICKING ON MEDIUM BRUSH SIZE
+        }
+
+    }
+
+
 }
