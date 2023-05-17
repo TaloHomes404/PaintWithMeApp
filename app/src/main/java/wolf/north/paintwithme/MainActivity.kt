f5e2643c8b199efac2b4e458b4acff6d1497abbe
Bartek <bartlomiej00wilk@gmail.com>
2023-05-17 16:07:53 +0200

    XML STWORZENIE IKON ROZMIARÓW PIÓRA DODANIE DO MAIN FUNKCJONALNOŚCI KTÓRA POZWOLI NA WYBRANIE ROZMIARU PIÓRA OKNO DIALOGOWE - NOWE dialog_brush_size.xml

--- app/src/main/java/wolf/north/paintwithme/MainActivity.kt
+++ app/src/main/java/wolf/north/paintwithme/MainActivity.kt
@@ -3,6 +3,7 @@ package wolf.north.paintwithme
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setBrushSize(20.toFloat()) //DEFAULT BRUSH SIZE ON START
+
+        val imgBttnBrushSelector : ImageButton = findViewById(R.id.imgbttn_brushSizeSelector)
+        imgBttnBrushSelector.setOnClickListener {
+            displayBrushSizeDialog()
+        }
     }
 
+    //THIS METHOD IS DIALOG WINDOW DISPLAYED AFTER CLICKING ON BRUSH ICON,
+    //FOR EACH BUTTON (small,medium,big) WE USE METHOD setBrushSize AND CHANGE ITS VALUE
+    //Dialog.dismiss() is important as Dialog.show() to make dialog works proper
     private fun displayBrushSizeDialog(){
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size) // SET HOW DIALOG SHOULD LOOK (CREATED LAYOUT)
         brushDialog.setTitle("Brush size: ")
+        brushDialog.show()
         val smallImgBttn : ImageView = brushDialog.findViewById(R.id.imgbttn_brush_small)
         smallImgBttn.setOnClickListener {
             drawingView?.setBrushSize(10.toFloat()) //CHANGING BRUSH SIZE
@@ -33,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         val bigImgBttn : ImageView = brushDialog.findViewById(R.id.imgbttn_brush_big)
         bigImgBttn.setOnClickListener {
             drawingView?.setBrushSize(30.toFloat()) //CHANGING BRUSH SIZE
-            brushDialog.dismiss() // HIDE DIALOG AFTER CLICKING ON MEDIUM BRUSH SIZE
+            brushDialog.dismiss() // HIDE DIALOG AFTER CLICKING ON BIG BRUSH SIZE
         }
 
     }
